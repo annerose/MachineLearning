d97da2638649af4be4d6b57b2e56d8ff35a45588
annerose <fengr10@gmail.com>
2017-06-17 15:42:50 +0800

    modify as review

--- 02Statistics/testsa.py
+++ 02Statistics/testsa.py
@@ -24,8 +24,19 @@ def calcSumSquarDiff (arr):
 def calcSamleStandDevia (arr):
     return math.sqrt(calcSumSquarDiff(arr)  / (len(arr) - 1))
 
+
+
+#  	standard error of the mean difference
+def calcStandErrorMean (arr):
+    return calcSamleStandDevia (arr) / math.sqrt(len(arr))
+
+# 求t统计值
+def calcTSat(arr):
+    return  calcMean(arr) / calcStandErrorMean(arr);
+
+
 # 求均值标准差
-def calcStandErrorMean(arr1, arr2):
+def calcStandErrorMeanGroup(arr1, arr2):
     s1 = calcSamleStandDevia(arr1)
     s2 = calcSamleStandDevia(arr2)
 
@@ -46,7 +57,7 @@ def calcStandErrorbyPool (arr1, arr2):
     print 'StandErrorbyPool = ', se
 
 #  求t-test 统计值
-def calcTSat(arr1, arr2):
+def calcTSatGroup(arr1, arr2):
 
     # 均值标准差
     standErrMean = calcStandErrorMean(arr1, arr2)
@@ -54,7 +65,8 @@ def calcTSat(arr1, arr2):
     return  (calcMean(arr1) -  calcMean(arr2)) / standErrMean
 
 
-
+def calcConfidenceInterval(mean, t_critical, sem):
+    return mean - t_critical * sem, mean + t_critical * sem
 
 
 
@@ -65,22 +77,27 @@ arrCongruent   = [12.079,16.791,9.564,8.63,14.669,12.238,14.692,8.987,9.401,14.4
 # 文字和颜色不一致的识别时间
 arrIncongruent = [19.278,18.741,21.214,15.687,22.803,20.878,24.572,17.394,20.762,26.282,24.524,18.644,17.51,20.33,35.255,22.158,25.139,20.429,17.425,34.288,23.894,17.96,22.058,21.157]
 
+# 差值
+arrDiff = [-7.199,-1.95,-11.65,-7.057,-8.134,-8.64,-9.88,-8.407,-11.361,-11.802,-2.196,-3.346,-2.437,-3.401,-17.055,-10.028,-6.644,-9.79,-6.081,-21.919,-10.95,-3.727,-2.348,-5.153]
 
 
+print 'mean =', calcMean(arrDiff)
+print 'S =',calcSamleStandDevia(arrDiff)
+print 'SEM = ', calcStandErrorMean(arrDiff)
+print 't-stat = ', calcTSat(arrDiff)
+print 'CI = ', calcConfidenceInterval(calcMean(arrDiff), 2.069, calcStandErrorMean(arrDiff))
+#tSta = calcTSat(arrCongruent, arrIncongruent)
 
-# t test 统计值
-tSta = calcTSat(arrCongruent, arrIncongruent)
+# print 't test sats =', tSta
 
-print 't test sats =', tSta
-
-# 自由度
-df = len(arrCongruent) + len(arrIncongruent) - 2;
-
-print 'df =', df
-
-# t test 临界值 alpha = 0.05, one tail, df46, tc = 1.684 ~ 1.676
-tCrictal = -1.676
-
-# null hypothesis
-h0 = (tSta <  tCrictal)
-print 'h0 =', h0
+# # 自由度
+# df = len(arrCongruent) + len(arrIncongruent) - 2;
+#
+# print 'df =', df
+#
+# # t test 临界值 alpha = 0.05, one tail, df46, tc = 1.684 ~ 1.676
+# tCrictal = -1.676
+#
+# # null hypothesis
+# h0 = (tSta <  tCrictal)
+# print 'h0 =', h0
